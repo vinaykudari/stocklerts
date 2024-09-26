d7b2806f1dfa45c15a5ea33a0c9f2a72cf06c1d6
Vinay Kudari <vinay@braveorbit.io>
2024-09-25 20:09:51 -0700

    feat: setup ci/cd

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# shellcheck disable=SC2164
-cd ~/code/stocklerts/
-
-git pull origin main
-
-FINNHUB_API_KEY=$FINNHUB_API_KEY ENCRYPT_KEY=$ENCRYPT_KEY docker-compose up --build -d
---
+++ webhook_handler.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, abort
+import hmac
+import hashlib
+import os
+import docker
+
+app = Flask(__name__)
+
+WEBHOOK_SECRET = os.environ.get('GH_WEBHOOK_SECRET')
+
+
+def is_valid_signature(payload_body, signature):
+    expected_signature = hmac.new(
+        key=WEBHOOK_SECRET.encode(),
+        msg=payload_body,
+        digestmod=hashlib.sha256
+    ).hexdigest()
+    return hmac.compare_digest(f"sha256={expected_signature}", signature)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    signature = request.headers.get('X-Hub-Signature-256')
+    if not signature:
+        abort(400, 'X-Hub-Signature-256 header is missing')
+
+    if not is_valid_signature(request.data, signature):
+        abort(401, 'Invalid signature')
+
+    if request.json['ref'] == 'refs/heads/main':
+        client = docker.from_env()
+
+        try:
+            app_container = client.containers.get('stocklerts-app')
+            print('Updating the codebase')
+
+            # Execute git pull
+            exit_code, output = app_container.exec_run('git pull origin main')
+            if exit_code != 0:
+                print(f'Error pulling latest code: {output.decode()}')
+                abort(500, 'Failed to pull the latest code')
+
+            print('Restarting stocklerts')
+            app_container.restart()
+
+        except docker.errors.NotFound:
+            print('Error: Container not found')
+            abort(500, 'Container not found')
+
+        except Exception as e:
+            print(f'Error: {e}')
+            abort(500, 'Internal server error')
+
+        return 'OK', 200
+
+    return 'Not main branch', 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
